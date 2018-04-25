fc43a3b88efa80593f822a76c8bb046e719960ce
Dave Snowberger <git@fartherout.org>
2018-04-25 14:17:20 -0700

    added models

---
+++ imagersite/imager_images/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import User
+from sorl.thumbnail import ImageField
+
+
+class Album(models.Model):
+    """Photo album model."""
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='albums')
+    cover = models.ForeignKey(
+        'Photo',
+        on_delete=models.CASCADE,
+        related_name='+',
+        null=True,
+        blank=True)
+    photos = models.ManyToManyField('Photo', blank=True)
+    title = models.CharField(max_length=1024, default='Untitled')
+    description = models.TextField(blank=True, null=True)
+    date_created = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
+    date_published = models.DateField(blank=True, null=True)
+    published = models.CharField(
+            max_length=7,
+            choices=(
+                ('PRIVATE', 'Private'),
+                ('SHARED', 'Shared'),
+                ('PUBLIC', 'Public'),))
+
+    def __str__(self):
+        return self.title
+
+
+class Photo(models.Model):
+    """Photo model."""
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='photos')
+    albums = models.ManyToManyField(Album, blank=True)
+    image = ImageField(upload_to='images')
+    title = models.CharField(max_length=1024, default='Untitled')
+    description = models.TextField(blank=True, null=True)
+    date_uploaded = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
+    date_published = models.DateField(blank=True, null=True)
+    published = models.CharField(
+            max_length=7,
+            choices=(
+                ('PRIVATE', 'Private'),
+                ('SHARED', 'Shared'),
+                ('PUBLIC', 'Public'),))
+
+    def __str__(self):
+        return self.title
