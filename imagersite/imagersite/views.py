3147c589251214ce41a56bf2fde0b44ef572596b
Dave Snowberger <git@fartherout.org>
2018-04-24 20:36:27 -0700

    styled a bit

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 
 def home_view(request):
     '''Default "home" route'''
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'title': 'ImagerSite'})
 
 
 def user_profile_view(request):
     '''Default landing page after login.'''
-    return render(request, 'user_profile.html')
+    return render(
+        request, 'user_profile.html', {'title': 'ImagerSite > User Profile'})
