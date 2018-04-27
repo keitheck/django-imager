e87ea668958b3d391ee9bf6be522b7b46e17e8f8
Dave Snowberger <git@fartherout.org>
2018-04-26 17:39:29 -0700

    created user profile and library views and routes

--- imagersite/imager_profile/urls.py
+++ imagersite/imager_profile/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import profile_view
+from .views import profile_view, library_view
 
 urlpatterns = [
     path('', profile_view, name='profile'),
-    path('<str:username>', profile_view, name='named_profile')
+    path('<str:username>', profile_view, name='named_profile'),
+    path('<str:username>/library', library_view, name='library'),
 ]
--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -2,23 +2,51 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import ImagerProfile
 from imager_images.models import Photo, Album
 
-def profile_view(request, username=None):
-    owner = False
 
+def profile_view(request, username=None):
     if not username:
         username = request.user.get_username()
-        # owner = True
         if username == '':
             return redirect('auth_login')
 
     profile = get_object_or_404(ImagerProfile, user__username=username)
+    photos = Photo.objects.filter(user__username=username)
+    public_photos = photos.filter(published='PUBLIC')
+    shared_photos = photos.filter(published='SHARED')
+    private_photos = photos.filter(published='PRIVATE')
+
+    albums = Album.objects.filter(user__username=username)
+    public_albums = albums.filter(published='PUBLIC')
+    shared_albums = albums.filter(published='SHARED')
+    private_albums = albums.filter(published='PRIVATE')
+
+    context = {
+        'profile': profile,
+        'public_photos': public_photos,
+        'shared_photos': shared_photos,
+        'private_photos': private_photos,
+        'public_albums': public_albums,
+        'shared_albums': shared_albums,
+        'private_albums': private_albums,
+    }
+
+    return render(request, 'imager_profile/profile.html', context)
 
 
+def library_view(request, username=None):
+    owner = False
+    if not username:
+        username = request.user.get_username()
+        owner = True
+        if username == '':
+            return redirect('auth_login')
 
-def library_view():
-    pass
+    albums = Album.objects.all().filter(user__username=username)
 
+    if not owner:
+        albums = albums.all().filter(published='PUBLIC')
 
-        
-
-
+    context = {
+        'albums': albums,
+    }
+    return render(request, 'imager_profile/library.html', context)
