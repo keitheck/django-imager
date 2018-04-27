44f53b341544031619ecb8dd5fe3f044376bd0b5
Dave Snowberger <git@fartherout.org>
2018-04-26 22:00:57 -0700

    added photo gallery, did some tests

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import photo_view, photo_gallery_view, album_view, album_gallery_view, library_view
+from .views import photo_view,\
+        photo_gallery_view, album_view, album_gallery_view, library_view
 
 urlpatterns = [
     path('photos/<photo_id>', photo_view, name='photo'),
     path('photos', photo_gallery_view, name='photo_gallery'),
     path('albums/<album_id>', album_view, name="album"),
-    path('albums', album_gallery_view, name="album_gallery7"),
+    path('albums', album_gallery_view, name="album_gallery"),
     path('library', library_view, name='library'),
 ]
--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from .models import Album, Photo
-from sorl import thumbnail
 
 
 def photo_view(request, photo_id):
@@ -9,14 +8,20 @@ def photo_view(request, photo_id):
     context = {}
     if photo.user.username == username or photo.published == 'PUBLIC':
         context['photo'] = photo
-        
+
     return render(request, 'imager_images/photo.html', context)
 
 
-def photo_gallery_view():
-    pass
-    
-    
+def photo_gallery_view(request):
+    username = request.user.get_username()
+    gallery = Photo.objects.filter(
+                user__username=username).filter(published='PUBLIC')
+    context = {
+        'gallery': gallery,
+    }
+    return render(request, 'imager_images/photo_gallery.html', context)
+
+
 def album_view(request, album_id):
     album = Album.objects.filter(id=album_id).first()
     username = request.user.get_username()
@@ -29,11 +34,13 @@ def album_view(request, album_id):
 
 def album_gallery_view(request):
     username = request.user.get_username()
-    gallery = Album.objects.filter(user__username=username).filter(published='PUBLIC')
+    gallery = Album.objects.filter(
+                user__username=username).filter(published='PUBLIC')
     context = {
         'gallery': gallery,
     }
     return render(request, 'imager_images/album_gallery.html', context)
+
 
 def library_view(request, username=None):
     owner = False
