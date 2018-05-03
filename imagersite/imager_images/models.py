36ab4ef2f11e16ab7392a50df7f103811c2eb917
keitheck <eckertkeith@gmail.com>
2018-05-03 15:56:52 -0700

    revert relationships to previous state

--- imagersite/imager_images/models.py
+++ imagersite/imager_images/models.py
@@ -9,11 +9,13 @@ class Album(models.Model):
         User, on_delete=models.CASCADE, related_name='albums')
     cover = models.ForeignKey(
         'Photo',
-        on_delete=models.SET_NULL,
+        # on_delete=models.SET_NULL,
+        on_delete=models.CASCADE,
         related_name='+',
         null=True,
         blank=True)
-    photos = models.ManyToManyField('Photo', related_name='+', blank=True)
+    # photos = models.ManyToManyField('Photo', related_name='+', blank=True)
+    photos = models.ManyToManyField('Photo', related_name='albums', blank=True)
     title = models.CharField(max_length=1024, default='Untitled')
     description = models.TextField(blank=True, null=True)
     date_created = models.DateField(auto_now_add=True)
@@ -35,7 +37,7 @@ class Photo(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='photos')
     image = ImageField(upload_to='images')
-    albums = models.ManyToManyField('Album', related_name='+', blank=True)
+    # albums = models.ManyToManyField('Album', related_name='+', blank=True)
     title = models.CharField(max_length=1024, default='Untitled')
     description = models.TextField(blank=True, null=True)
     date_uploaded = models.DateField(auto_now_add=True)
