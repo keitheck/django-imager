3d716ae4b45be480df4bc65f70b078096cea44c8
Dave Snowberger <git@fartherout.org>
2018-05-03 17:16:07 -0700

    updated production.py to move email settings into env

--- imagersite/imagersite/production.py
+++ imagersite/imagersite/production.py
@@ -1,4 +1,32 @@
 from .settings import *
+import os
+
 
 DEBUG = False
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 TEMPLATE_DEBUG = False
+EMAIL_HOST = os.environ.get('EMAIL_HOST', '')
+EMAIL_PORT = os.environ.get('EMAIL_PORT', '')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', True)
+EMAIL_TIMEOUT = os.environ.get('EMAIL_TIMEOUT', None)
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
