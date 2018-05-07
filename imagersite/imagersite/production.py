eb960811b99fb5bac5eebc4ad897e38b39ed157c
Dave Snowberger <git@fartherout.org>
2018-05-06 19:47:26 -0700

    reverted production.py since it seems to break the email for some reason

--- imagersite/imagersite/production.py
+++ imagersite/imagersite/production.py
@@ -1,16 +1,15 @@
 from .settings import *
-import os
 
 
 DEBUG = False
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 TEMPLATE_DEBUG = False
-EMAIL_HOST = os.environ.get('EMAIL_HOST', '')
-EMAIL_PORT = os.environ.get('EMAIL_PORT', '')
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', True)
-EMAIL_TIMEOUT = os.environ.get('EMAIL_TIMEOUT', None)
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'imagersite401@gmail.com'
+EMAIL_HOST_PASSWORD = 'KkC4t!Nf<'
+EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = None
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 LOGGING = {
     'version': 1,
